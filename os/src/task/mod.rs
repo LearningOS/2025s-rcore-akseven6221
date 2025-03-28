42ace5dd733646c6e596db29ed4b161abd03c197
alioth <2178078908@qq.com>
2025-03-28 13:44:51 +0800

    lab1 pass

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -54,6 +54,7 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
+            syscall_times: [0; 500],
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
@@ -135,6 +136,20 @@ impl TaskManager {
             panic!("All applications completed!");
         }
     }
+
+    fn get_syscall_times(&self, sys_id: usize) -> isize {
+        let inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        let arr = inner.tasks[current].syscall_times.clone();
+        arr[sys_id]
+    }
+
+    fn add_syscall_times(&self, sys_id: usize) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].syscall_times[sys_id] += 1;
+    }
+
 }
 
 /// Run the first task in task list.
@@ -168,4 +183,14 @@ pub fn suspend_current_and_run_next() {
 pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
+}
+
+/// get syscall times
+pub fn get_syscall_times(sys_id: usize) -> isize {
+    TASK_MANAGER.get_syscall_times(sys_id)
+}
+
+/// add syscall times
+pub fn add_syscall_times(sys_id: usize) {
+    TASK_MANAGER.add_syscall_times(sys_id);
 }
